6ff612be51a9fcd44b413e9680a12cc646178e30
abhipatel0211 <abhiptl0123@gmail.com>
2023-08-20 01:47:05 +0530

    vercel in frontend

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 const config = {
-  REACT_APP_BACKEND_URL: "https://chatwhiz-backend.vercel.app/",
+  REACT_APP_BACKEND_URL: "https://chatwhiz-backend.vercel.app",
   REACT_APP_WS_URL: "chatwhiz-backend.vercel.app",
 };
 
